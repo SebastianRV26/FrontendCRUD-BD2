70c702b6ebfea5c070b6ce13e9968ae41cdda9d7
Jairo Pacheco <pachecocamposjairo@gmail>
2020-10-15 21:25:05 -0600

    loging Jairo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatFormFieldModule,
     MatButtonModule,
     ReactiveFormsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MatSelectChange } from '@angular/material/select';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -10,20 +12,46 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
+  seleccionada: String;
+  puerto:String;
 
-  constructor(private router: Router, private formBuilder: FormBuilder) { }
+  constructor(private router: Router, private formBuilder: FormBuilder, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      db_name: ['', Validators.required],
       user: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
+      server: ['', Validators.required],
+      db_name: ['', Validators.required],
+      port: ['', Validators.required],
+      driver: ['', Validators.required]
     });
   }
 
   login() {
-    console.log(this.loginForm.value.user);
-    this.router.navigateByUrl('/information');
+    this.loginForm.value.driver = this.seleccionada;
+    var split = this.loginForm.value.server.split(":",2);
+    if(this.loginForm.value.driver === "mssql"){
+      this.loginForm.removeControl("port");
+      this.loginForm.value.driver = this.seleccionada;
+      this.loginForm.value.server = split[0];
+      this.router.navigateByUrl('/information');
+    }else{
+      if(split[1] == undefined){
+        this._snackBar.open(" Debe de colocar el puerto junto al server ", 'Cerrar', {
+          duration: 2000,
+        });
+      }else{
+        this.loginForm.value.server = split[0];
+        this.loginForm.value.port = split[1];
+        this.router.navigateByUrl('/information');
+      }
+    }
+    console.log(this.loginForm.value);
   }
 
+  changeRatio(event: MatSelectChange) {
+    this.seleccionada = event.value;
+    console.log(this.seleccionada);
+  }
 }
